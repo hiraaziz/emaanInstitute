6e58265191277c97da53f4ff3841bfd5232ef2e6
hiraaziz <hiraaziz1998@gmail.com>
2023-02-21 13:52:18 +0500

    Create lastcard.jsx

---
+++ components/applycard/lastcard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+const Lastcard = () => {
+  return (
+    <div className="w-4/5 bg-black h-64 flex justify-center items-center text-white m-auto">
+      <h1 className="font-black text-3xl w-3/5">
+        BECOME A PART OF ONE OF THE PAKISTAN's TOPRANKED UNIVERSITY
+      </h1>
+      <button className="bg-gradient-to-r from-darkpink to-lightpink rounded p-4 font-semibold">
+        APPLY NOW
+      </button>
+    </div>
+  );
+};
+
+export default Lastcard;
