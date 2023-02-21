51c434c268b9701922bbdfd568dcd3a4296192d9
hiraaziz <hiraaziz1998@gmail.com>
2023-02-21 13:51:58 +0500

    Create cardlist.js

---
+++ components/cards/cardlist.js
@@ -0,0 +1,38 @@
+export let cardlist = [
+    {
+        img:'ai.jpg',
+        tags:['html','css','javascript'],
+        title:'Bachelors of Business Admisnitration',
+        years:4
+    },
+    {
+        img:'automation.jpg',
+        tags:['html','css','javascript'],
+        title:'Bachelors Business and Commerce',
+        years:2
+    },
+    {
+        img:'cloud.jpg',
+        tags:['html','css','javascript'],
+        title:'Database Managemnet System',
+        years:2
+    },
+    {
+        img:'web3.jpg',
+        tags:['html','css','javascript'],
+        title:'Bachelors of science in Computer Science',
+        years:4
+    },
+    {
+        img:'web3.jpg',
+        tags:['html','css','javascript'],
+        title:'Web Design and Development',
+        years:2
+    },
+    {
+        img:'web3.jpg',
+        tags:['html','css','javascript'],
+        title:' Bachelors Computer Networking',
+        years:2
+    }
+]
